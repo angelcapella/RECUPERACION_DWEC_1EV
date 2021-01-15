56f8b3eca6d929f0576464469df509b49f40bb96
DESKTOP-G7I7M5O\kape9 <angelcapellaruiz@gmail.com>
2021-01-15 12:21:11 +0100

    0.2

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //1
+const texto = document.getElementsByClassName('input-container').item[0].textArea
+const boton = document.createElement('button');
+
+
+
+    //2
+
+
+
+    //3
+
+
+
+    //4
+    const cambiarcolor = document.getElementsByClassName('text-container');
+    document.body.appendChild(cambiarcolor);
+
+    button2.addEventListener('mouseover', () => {
+        cambiarcolor.textContent = seccion2.textContent;
+    });
+
+
+    //5
+
+
+
+
+
+});
