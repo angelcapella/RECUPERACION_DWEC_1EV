eb9fc80723cd40534c08420ecb5b49e76df10eb8
DESKTOP-G7I7M5O\kape9 <angelcapellaruiz@gmail.com>
2021-01-15 13:26:56 +0100

    0.3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+///const input=document.getElementsByClassName('input-container').item(0);
+const botonCo=document.getElementById('buttons-container');
     //1
-const texto = document.getElementsByClassName('input-container').item[0].textArea
-const boton = document.createElement('button');
+
+
+const boton1 = document.createElement('button');
+const boton2 = document.createElement('button');
+botonCo.appendChild(boton1) 
+botonCo.appendChild(boton2) 
+boton1.innerText='contar palabras';
+boton2.innerText='restablecer';
+const span=document.createElement('span');
+botonCo.appendChild(span);
+
+boton1.addEventListener('click', () => {
+    const countWord = (texto) => {
+        let count = 0;
+        const text = texto.textContent.replace(',', '');
+        const textContent = text.replace('.', '');
+        const words = textContent.split('');
+        for (let i = 0; i < words.length; i++) {
+            if (words[i].length > 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+    const result = () => {
+        span.textContent = countWord(botonCo);
+       
+    }
+        result();
+
+});
+boton2.addEventListener('click', () => {
+
+    
+
+});
+
 
 
 
@@ -15,17 +51,24 @@ const boton = document.createElement('button');
 
 
     //4
-    const cambiarcolor = document.getElementsByClassName('text-container');
-    document.body.appendChild(cambiarcolor);
 
-    button2.addEventListener('mouseover', () => {
-        cambiarcolor.textContent = seccion2.textContent;
+    const texto = document.getElementsByClassName('text-container');
+    document.body.appendChild(texto);
+
+    texto.addEventListener('mouseover', () => {
+        document.body.texto.style.fontSize='100px'
     });
+        
+
 
 
     //5
 
+    let nombre= document.createElement('p');
+    texto.appendChild(nombre);
 
+    let numero= document.createElement('p');
+    texto.appendChild(numero);
 
 
 
